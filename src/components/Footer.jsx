ae79dcd33ed36793afd62d081cd3869ea8912476
Saransh gupta <Saranshgupta123456789.sg.sg@gmail.com>
2024-10-27 22:28:26 +0530

    second

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Saransh Gupta</a>
+              <a  href="https://github.com/SaranshGupta123/erommerce" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Saransh Gupta</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/SaranshGupta123" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
